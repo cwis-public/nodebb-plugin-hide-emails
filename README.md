fe76a42aac2816c4fb9d7f56c761d6ee800cae98
Christopher Allène <christopher.allene@piximedia.fr>
2016-05-09 20:55:36 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# nodebb-plugin-hide-emails
+Hide email from all users, except admins. By default, Global Moderators are able to see emails, which this plugin prevents.
+
+To use, install this module to your NodeBB setup, and activate the plugin.
---
+++ index.js
@@ -0,0 +1,45 @@
+(function() {
+
+"use strict";
+
+var NodeBB = require.main;
+var accountHelpers = NodeBB.require('./src/controllers/accounts/helpers.js');
+var winston = NodeBB.require('winston');
+
+exports.init = function(params, callback) {
+	if(accountHelpers.getUserDataByUserSlug._emailHidden) {
+		return callback();
+	}
+
+	var oldGetUserDataByUserSlug = accountHelpers.getUserDataByUserSlug;
+	accountHelpers.getUserDataByUserSlug = function(userslug, callerUID, callback) {
+		oldGetUserDataByUserSlug(userslug, callerUID, function(err, userData) {
+			if(err) {
+				return callback(err);
+			}
+			var isAdmin = userData.isAdmin;
+			var isSelf = userData.isSelf;
+			winston.warn("getUserDataByUserSlug(" + userslug + "," + callerUID + ")");
+			winston.warn("isAdmin: " + isAdmin);
+			winston.warn("isSelf: " + isSelf);
+			winston.warn("userData.email: " + userData.email);
+			winston.warn("userData.emailClass: " + userData.emailClass);
+
+			if(!isAdmin && !isSelf) {
+				winston.warn("Hiding email address");
+				userData.email = '';
+				userData.emailClass = 'hide';
+			}
+
+			winston.warn("*result* userData.email: " + userData.email);
+			winston.warn("*result* userData.emailClass: " + userData.emailClass);
+
+			return callback(null, userData);
+		});
+	};
+	accountHelpers.getUserDataByUserSlug._emailHidden = true;
+	winston.info("Now hiding emails from non-admin");
+	return callback();
+};
+
+})();
